810ddb5cccc01d98401c57195a0fe5721002cb5b
candinho <candinho.junior@supero.com.br>
2022-03-30 15:24:09 -0300

    add a time converter

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,8 +1,11 @@
 import style from "./Timer.module.scss";
 import Button from "../Button";
 import Clock from "./Clock";
+import { timeToSeconds } from "../../common/utils/date";
 
 export default function Timer() {
+  console.log(`conversão: `, timeToSeconds("01:01:01"));
+
   return (
     <div className={style.timer}>
       <p className={style.title}>Escolha um card e inicie o cronômetro</p>
